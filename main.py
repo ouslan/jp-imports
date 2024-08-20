555ff3e263d82174bb55999af526b45b2e6012e3
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-08-20 17:59:41 +0000

    corrected path
    change path to specify that it is a directory

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.jp_imports.data_process import DataProcess
 
 def main() -> None:
-    save_path = "data"
+    save_path = "data/"
     DataProcess(saving_dir=save_path, agriculture=True, debug=True)
 
 if __name__ == "__main__":
