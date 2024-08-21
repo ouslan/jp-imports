1ad281a359faef3912e75d8ec59968e73f2c5f93
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-08-21 11:51:39 -0400

    renamed folder

--- src/jp_imports/data_pull.py
+++ src/jp_imports/data_pull.py
@@ -1,8 +1,8 @@
 from urllib.request import urlretrieve
 from urllib.error import URLError
+from dotenv import load_dotenv
 import polars as pl
 import requests
-from dotenv import load_dotenv
 import zipfile
 import os
 
@@ -10,21 +10,31 @@ load_dotenv()
 
 class DataPull:
 
-    def __init__(self, saving_dir:str, debug:bool=False) -> None:
+    def __init__(self, saving_dir:str, state_code:str, hts:bool=False, jp_instance:bool=False, debug:bool=False) -> None:
+
+        if jp_instance and not state_code == "PR":
+            raise ValueError("JP instance must be for Puerto Rico only")
+
         self.saving_dir = saving_dir
+        self.state_code = state_code
+        self.jp_instance = jp_instance
         self.debug = debug
+
+        if hts:
+            self.pull_census_hts(end_year=2023, start_year=2013, exports=True, state="PR", saving_dir=self.saving_dir)
+            self.pull_census_hts(end_year=2023, start_year=2013, exports=False, state="PR", saving_dir=self.saving_dir)
+        else:
+            self.pull_census_naics(end_year=2023, start_year=2013, exports=True, state="PR", saving_dir=self.saving_dir)
+            self.pull_census_naics(end_year=2023, start_year=2013, exports=False, state="PR", saving_dir=self.saving_dir)
+
+    def pull_int_org(self):
+
+        if not os.path.exists(self.saving_dir + "raw/import.csv") and not os.path.exists(self.saving_dir + "raw/export.csv") or not os.path.exists(self.saving_dir + "processed/imp_exp.parquet"):
+            self.pull_file(url="http://www.estadisticas.gobierno.pr/iepr/LinkClick.aspx?fileticket=JVyYmIHqbqc%3d&tabid=284&mid=244930", filename=(self.saving_dir + "raw/tmp.zip"))
         if not os.path.exists(self.saving_dir + "raw"):
             os.makedirs(self.saving_dir + "raw")
         if not os.path.exists(self.saving_dir + "processed"):
             os.makedirs(self.saving_dir + "processed")
-        self.pull_imp_exp()
-        self.pull_census(end_year=2023, start_year=2013, exports=True, state="PR", saving_dir=self.saving_dir)
-        self.pull_census(end_year=2023, start_year=2013, exports=False, state="PR", saving_dir=self.saving_dir)
-
-    def pull_imp_exp(self):
-
-        if not os.path.exists(self.saving_dir + "raw/import.csv") and not os.path.exists(self.saving_dir + "raw/export.csv") or not os.path.exists(self.saving_dir + "processed/imp_exp.parquet"):
-            self.pull_file(url="http://www.estadisticas.gobierno.pr/iepr/LinkClick.aspx?fileticket=JVyYmIHqbqc%3d&tabid=284&mid=244930", filename=(self.saving_dir + "raw/tmp.zip"))
 
             # Extract the zip file
             with zipfile.ZipFile(self.saving_dir + "raw/tmp.zip", "r") as zip_ref:
@@ -65,7 +75,10 @@ class DataPull:
                 if self.debug:
                     print("\033[1;33mWARNING:  \033[0m" + f"Could not download {filename}")
 
-    def pull_census(self, end_year:int, start_year:int, exports:bool, state:str, saving_dir:str) -> None:
+    def pull_int_jp(self) -> None:
+        pass
+
+    def pull_census_hts(self, end_year:int, start_year:int, exports:bool, state:str, saving_dir:str) -> None:
         empty_df = [
             pl.Series("date", dtype=pl.Datetime),
             pl.Series("census_value", dtype=pl.Int64),
@@ -75,7 +88,7 @@ class DataPull:
             pl.Series("contry_code", dtype=pl.String),
         ]
         census_df = pl.DataFrame(empty_df)
-        if not os.path.exists(f"{saving_dir}/raw/census_{type}.parquet"):
+        if not os.path.exists(f"{saving_dir}/raw/census_hts_{type}.parquet"):
             base_url = "https://api.census.gov/data/timeseries/"
             key = os.getenv("CENSUS_API_KEY")
 
@@ -83,12 +96,12 @@ class DataPull:
                 param = 'CTY_CODE,CTY_NAME,ALL_VAL_MO,COMM_LVL,E_COMMODITY'
                 flow = "intltrade/exports/statehs"
                 naming = {"CTY_CODE": "contry_code", "CTY_NAME": "country_name", "ALL_VAL_MO": "census_value", "COMM_LVL": "comm_level", "E_COMMODITY": "commodity"}
-                saving_path = f"{saving_dir}/raw/census_exports.parquet"
+                saving_path = f"{saving_dir}/raw/census_hts_exports.parquet"
             else:
                 param = 'CTY_CODE,CTY_NAME,GEN_VAL_MO,COMM_LVL,I_COMMODITY'
                 flow = "intltrade/imports/statehs"
                 naming = {"CTY_CODE": "contry_code", "CTY_NAME": "country_name", "GEN_VAL_MO": "census_value", "COMM_LVL": "comm_level", "I_COMMODITY": "commodity"}
-                saving_path = f"{saving_dir}/raw/census_imports.parquet"
+                saving_path = f"{saving_dir}/raw/census_hts_imports.parquet"
  
             for year in range(start_year, end_year + 1):
 
@@ -98,15 +111,64 @@ class DataPull:
                 names = df.select(pl.col("column_0")).transpose()
                 df = df.drop("column_0").transpose()
                 df = df.rename(names.to_dicts().pop())
-                df = df.rename(naming)
                 df = df.with_columns(date=(pl.col("time") + "-01").str.to_datetime("%Y-%m-%d"))
                 df = df.select(pl.col("date", "census_value", "comm_level", "commodity", "country_name", "contry_code"))
                 df = df.with_columns(pl.col("census_value").cast(pl.Int64))
                 census_df = pl.concat([census_df, df], how="vertical")
+
                 if self.debug:
                     print("\033[0;32mINFO: \033[0m" + f"Downloaded {year} data")
 
             census_df.write_parquet(saving_path)
         else:
+
+            if self.debug:
+                print("\033[0;36mNOTICE: \033[0m" + "Census data already exists, skipping download")
+
+    def pull_census_naics(self, end_year:int, start_year:int, exports:bool, state:str, saving_dir:str) -> None:
+        empty_df = [
+            pl.Series("date", dtype=pl.Datetime),
+            pl.Series("census_value", dtype=pl.Int64),
+            pl.Series("comm_level", dtype=pl.String),
+            pl.Series("naics_desc", dtype=pl.String),
+            pl.Series("naics_code", dtype=pl.String),
+            pl.Series("country_name", dtype=pl.String),
+            pl.Series("contry_code", dtype=pl.String),
+        ]
+        if not os.path.exists(f"{saving_dir}/raw/census_naics{type}.parquet"):
+            census_df = pl.DataFrame(empty_df)
+            base_url = "https://api.census.gov/data/timeseries/"
+            key = os.getenv("CENSUS_API_KEY")
+
+            if exports:
+                param = 'CTY_CODE,CTY_NAME,ALL_VAL_MO,COMM_LVL,E_NAICS'
+                flow = "intltrade/exports/statenaics"
+                param = 'CTY_CODE,CTY_NAME,ALL_VAL_MO,COMM_LVL,NAICS'
+                saving_path = f"{saving_dir}/raw/census_naics_exports.parquet"
+            else:
+                param = 'CTY_CODE,CTY_NAME,GEN_VAL_MO,COMM_LVL,I_NAICS'
+                flow = "intltrade/imports/statenaics"
+                saving_path = f"{saving_dir}/raw/census_naics_imports.parquet"
+
+            for year in range(start_year, end_year + 1):
+
+                url = f"{base_url}{flow}?get={param}&STATE={state}&key={key}&time={year}"
+                r = requests.get(url).json()
+                df = pl.DataFrame(r)
+                names = df.select(pl.col("column_0")).transpose()
+                df = df.drop("column_0").transpose()
+                df = df.rename(names.to_dicts().pop())
+                df = df.with_columns(date=(pl.col("time") + "-01").str.to_datetime("%Y-%m-%d"))
+                df = df.select(pl.col("date", "census_value", "comm_level", "naics_desc", "naics_code", "country_name", "contry_code"))
+                df = df.with_columns(pl.col("census_value").cast(pl.Int64))
+                census_df = pl.concat([census_df, df], how="vertical")
+
+                if self.debug:
+                    print("\033[0;32mINFO: \033[0m" + f"Downloaded {year} data")
+
+            census_df.write_parquet(saving_path)
+
+        else:
+
             if self.debug:
                 print("\033[0;36mNOTICE: \033[0m" + "Census data already exists, skipping download")
