5b92a38ba14b4fb5b066f23cf5b24585585e2b0f
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-09-04 11:23:53 -0400

    finished instetute function and added census function

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-#from src.jp_imports.data_process import DataProcess
+from src.jp_imports.data_process import DataProcess
 from src.jp_imports.data_pull import DataPull
 
 def main() -> None:
     save_path = "data/"
-    #DataProcess(saving_dir=save_path, agriculture=True, debug=True)
-    DataPull(saving_dir=save_path, state_code="PR", instance="jp_instetute", debug=True)
+    df = DataProcess(saving_dir=save_path, instance="instetute", debug=True).process_int_jp(time="yearly", types="naics")
+    print(df)
+    #DataPull(saving_dir=save_path, state_code="PR", instance="jp_instetute", debug=True)
 
 if __name__ == "__main__":
     main()
