09b08dab65ea908b553494e1758d8c7a098cfdcd
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-09-16 15:55:34 -0400

    commit 5
    Signed-off-by: Alejandro Ouslan <alejandro.ouslan@upr.edu>

--- src/jp_imports/data_process.py
+++ src/jp_imports/data_process.py
@@ -12,11 +12,11 @@ class DataProcess(DataPull):
         self.instance = instance
         self.codes = json.load(open(self.saving_dir + "external/code_classification.json"))
 
-    def process_int_jp(self, time:str, types:str, group:bool=False):
+    def process_int_jp(self, time:str, types:str, group:bool=False) -> pl.LazyFrame:
         switch = [time, types]
         if group:
             #return self.process_cat(switch=switch)
-            print("Not implemented")
+            raise NotImplementedError("Grouping not implementedl")
         else:
             return self.process_data(switch)
 
